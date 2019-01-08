07f7c3fa8836e8e73493dcde46644537863b586f
phanssen <pvhanssen@gmail.com>
2019-01-08 18:21:38 -0500

    Initial Commit

---
+++ Honors ProjectTests/Honors_ProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  Honors_ProjectTests.swift
+//  Honors ProjectTests
+//
+//  Created by Paige Hanssen on 1/8/19.
+//  Copyright © 2019 Paige Hanssen. All rights reserved.
+//
+
+import XCTest
+@testable import Honors_Project
+
+class Honors_ProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
