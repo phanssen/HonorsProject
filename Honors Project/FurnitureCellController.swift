33fa6df843f0b71306a2d14e89da384d13422927
phanssen <pvhanssen@gmail.com>
2019-02-05 11:19:31 -0500

    Add 3D objects from file, remove objects from scene

---
+++ Honors Project/FurnitureCellController.swift
@@ -0,0 +1,14 @@
+//
+//  FurnitureCellController.swift
+//  Honors Project
+//
+//  Created by Paige Hanssen on 1/30/19.
+//  Copyright © 2019 Paige Hanssen. All rights reserved.
+//
+
+import UIKit
+
+class FurnitureCellController: UICollectionViewCell {
+    @IBOutlet weak var furnitureImageView: UIImageView!
+    @IBOutlet weak var furnitureLabel: UILabel!
+}
--- Honors Project/ViewController.swift
+++ Honors Project/ViewController.swift
@@ -12,20 +12,15 @@ import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
     var menuImage : UIImage = UIImage(named: "art.scnassets/menu_icon.png")!
+    var nodeModel: SCNNode!
+    let nodeName = "cherub"
 
     @IBOutlet var sceneView: ARSCNView!
-
     @IBOutlet weak var menuButton: UIButton!
-
     @IBOutlet var menuOptions: [UIButton]!
 
     @IBAction func menuAction(_ sender: UIButton) {
-        menuOptions.forEach{(option) in
-            UIView.animate(withDuration: 0.3, animations: ({
-                option.isHidden = !option.isHidden
-                self.view.layoutIfNeeded()
-            }))
-        }
+        menuControl()
     }
 
     override func viewDidLoad() {
@@ -36,19 +31,27 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
         // Set the view's delegate
         sceneView.delegate = self
+        sceneView.antialiasingMode = .multisampling4X
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
 
         // Set the scene to the view
         sceneView.scene = scene
+
+        let modelScene = SCNScene(named:
+            "art.scnassets/cherub/cherub.dae")!
+
+        nodeModel =  modelScene.rootNode.childNode(
+            withName: nodeName, recursively: true)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -56,37 +59,65 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
+
         // Pause the view's session
         sceneView.session.pause()
     }
 
+    // track screen touches on ARView and either add or remove object accordingly
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else {return}
-        let result = sceneView.hitTest(touch.location(in: sceneView), types: [ARHitTestResult.ResultType.featurePoint])
+        // get location of the touch
+        let location = touches.first!.location(in: sceneView)
 
-        guard let hitResult = result.last else {return}
-        let hitTransform = (hitResult.worldTransform)
-        let hitVector = SCNVector3Make(hitTransform.columns.3.x, hitTransform.columns.3.y, hitTransform.columns.3.z)
+        // test if an object was touched
+        var hitTestOptions = [SCNHitTestOption: Any]()
+        hitTestOptions[SCNHitTestOption.boundingBoxOnly] = true
+        let hitResults: [SCNHitTestResult]  =
+            sceneView.hitTest(location, options: hitTestOptions)
 
-        createShip(position: hitVector)
+        // an object has been touched and gets removed(?) -> turn this into "showDetails"
+        if let hit = hitResults.first {
+            if let node = getParent(hit.node) {
+                // prompt user to confirm they want to clear object
+                let alertPrompt = UIAlertController(title: "Remove Object", message: "Are you sure you want to remove this object?", preferredStyle: .alert)
+
+                // add yes and no options
+                alertPrompt.addAction(UIAlertAction(title: "No", style: .cancel, handler: { _ in
+                    return
+                }))
+                alertPrompt.addAction(UIAlertAction(title: NSLocalizedString("Yes", comment: "Default Action"), style: .default, handler: { _ in
+                        node.removeFromParentNode()
+                    }))
+
+                // display prompt
+                self.present(alertPrompt, animated: true, completion: nil)
+
+                return
+            }
+        }
+
+        // find a feature point and add an ARAnchor
+        let hitResultsFeaturePoints: [ARHitTestResult] =
+            sceneView.hitTest(location, types: .featurePoint)
+        if let hit = hitResultsFeaturePoints.first {
+            sceneView.session.add(anchor: ARAnchor(transform: hit.worldTransform))
+        }
     }
 
-    func createShip(position: SCNVector3) {
-        let sphereShape = SCNSphere(radius: 0.05)
+    // recursively search for parent node
+    func getParent(_ nodeFound: SCNNode?) -> SCNNode? {
+        if let node = nodeFound {
+            if node.name == nodeName {
+                return node
+            } else if let parent = node.parent {
+                return getParent(parent)
+            }
+        }
+        return nil
+    }
 
-        // set the color of the box...can use UIImage(named: "") for texture from an image
-        let material = SCNMaterial()
-        material.diffuse.contents = UIColor.green
-
-        sphereShape.materials = [material]
-
-        // create the scene node from the shape
-        let sphere = SCNNode(geometry: sphereShape)
-        sphere.position = position
-
-        // var shipNode = SCNNode(mdlObject: "art.scnassets/ship.scn")
-        sceneView.scene.rootNode.addChildNode(sphere)
+    @IBAction func handleLibraryButton(_ sender: Any) {
+        performSegue(withIdentifier: "toLibrary", sender: self)
     }
 
     // remove single object from the view
@@ -96,6 +127,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     // clear all objects from the scene; connected to Clear View from menu
     @IBAction func clearView() {
+        menuControl()
+
         // prompt user to confirm they want to clear view
         let alertPrompt = UIAlertController(title: "Clear View", message: "Are you sure you want to remove all objects from current view?", preferredStyle: .alert)
 
@@ -104,25 +137,58 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             return
         }))
         alertPrompt.addAction(UIAlertAction(title: NSLocalizedString("Yes", comment: "Default Action"), style: .default, handler: { _ in
-                let newScene = SCNScene(named: "art.scnassets/newscene.scn")!
-                self.sceneView.scene = newScene
+            self.sceneView.scene.rootNode.enumerateChildNodes { (node, stop) in
+                node.removeFromParentNode()
+            }
         }))
 
         // display prompt
         self.present(alertPrompt, animated: true, completion: nil)
     }
 
+    // open and close the menu
+    func menuControl() {
+        menuOptions.forEach{(option) in
+            UIView.animate(withDuration: 0.3, animations: ({
+                option.isHidden = !option.isHidden
+                self.view.layoutIfNeeded()
+            }))
+        }
+    }
+
     // MARK: - ARSCNViewDelegate
-    
+
 /*
     // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
-     
+
+        if !anchor.isKind(of: ARPlaneAnchor.self) {
+            DispatchQueue.main.async {
+                let modelClone = self.nodeModel.clone()
+                modelClone.position = SCNVector3Zero
+
+                // Add model as a child of the node
+                node.addChildNode(modelClone)
+            }
+        }
+
         return node
     }
-*/
-    
+ */
+
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if !anchor.isKind(of: ARPlaneAnchor.self) {
+            DispatchQueue.main.async {
+                let modelClone = self.nodeModel.clone()
+                modelClone.position = SCNVector3Zero
+
+                // Add model as a child of the node
+                node.addChildNode(modelClone)
+            }
+        }
+    }
+
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
         
@@ -138,3 +204,22 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
     }
 }
+
+
+
+//func createShip(position: SCNVector3) {
+//    let sphereShape = SCNSphere(radius: 0.05)
+//
+//    // set the color of the box...can use UIImage(named: "") for texture from an image
+//    let material = SCNMaterial()
+//    material.diffuse.contents = UIColor.green
+//
+//    sphereShape.materials = [material]
+//
+//    // create the scene node from the shape
+//    let sphere = SCNNode(geometry: sphereShape)
+//    sphere.position = position
+//
+//    // var shipNode = SCNNode(mdlObject: "art.scnassets/ship.scn")
+//    sceneView.scene.rootNode.addChildNode(sphere)
+//}
